0e624d5521229b1332dca1cddacd826d4fa41279
henderson-01 <261472068+henderson-01@users.noreply.github.com>
2026-07-31 17:32:02 +0100

    Updated now using Ruff formatting in this project

--- alien.py
+++ alien.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
@@ -11,7 +12,7 @@ class Alien(Sprite):
         self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load("images/alien.bmp")
         self.rect = self.image.get_rect()
 
         # start each new alien near the top left of the screen.
